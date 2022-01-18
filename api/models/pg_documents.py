18542f269b98a6709816a2b53d1ab8cddd86d968
ani <anisakhlian@gmail.com>
2022-01-18 15:33:46 +0400

    removed parent_id column, added order column in Documents model

--- api/models/pg_documents.py
+++ api/models/pg_documents.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Table, Column, DateTime, String, Boolean, ForeignKey, event, func
+from sqlalchemy import Table, Column, DateTime, String, Boolean, ForeignKey, event, func, Integer
 from sqlalchemy.dialects.postgresql import JSONB, UUID
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.mutable import MutableDict
@@ -20,7 +20,7 @@ class PgDocument(SqlalchemyHelper.Base, PGNormModel):
     body = Column(String, nullable=False)
     is_pinned = Column(Boolean, default=False)
     deleted_at = Column(DateTime, default=None, nullable=True)
-    parent_id = Column(UUID(), ForeignKey('documents.id', ondelete="CASCADE"))
+    order = Column(Integer, default=0, nullable=False)
 
     def __repr__(self):
         return f'Document<id: {self.id}, title: {self.title}>'
@@ -30,7 +30,7 @@ class PgDocument(SqlalchemyHelper.Base, PGNormModel):
             'id': self.id,
             'title': self.title,
             'body': self.body,
-            'parent_id': self.parent_id,
+            'order': self.order,
             'created_at': self.created_at.isoformat(),
             'updated_at': self.updated_at.isoformat(),
             'deleted_at': self.deleted_at.isoformat() if self.deleted_at else None,
